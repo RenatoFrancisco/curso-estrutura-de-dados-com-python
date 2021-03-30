ec4345738b3329e8f51dfeeaea40767be58d7945
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-03-29 22:49:13 -0300

    Lista ligada introduçao

---
+++ rota.py
@@ -0,0 +1,19 @@
+from ed.lista_ligada import ListaLigada, Celula
+
+class Loja:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+    def __repr__(self):
+        return "{}\n {}".format(self.nome, self.endereco)
+
+def main():
+    loja1 = Loja("Mercadinho", "Rua das Laranjeira, 12")
+    loja2 = Loja("Hortifruti", "Rua do Pomar, 300")
+    loja3 = Loja("Padaria", "Rua das Flores, 600")
+
+    lista = ListaLigada()
+    print(lista.quantidade)
+
+main()

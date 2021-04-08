9924d895513c1dd5af0052f55e843c8566fbd723
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-04-07 22:20:11 -0300

    Arvore Apresentação parte 2

--- ed/arvore.py
+++ ed/arvore.py
@@ -4,14 +4,30 @@ class ListaDeNodos(ListaDuplamenteLigada):
     def __init__(self):
         super().__init__()
 
+    def imprimir(self, nivel):
+        atual = self.inicio
+        for i in range(0, self.quantidade):
+            atual.conteudo.imprimir(nivel)
+            atual = atual.proximo
+
+
 class Nodo:
     def __init__(self, conteudo):
         self.conteudo = conteudo
         self.pai = None
         self.filhos = None
 
-    def imprimir(self):
-        print(self.conteudo)
+    def imprimir(self, nivel=0):
+        print('{}- {}'.format(' '*nivel, len(self.conteudo)))
+        if self.filhos:
+            self.filhos.imprimir(nivel+1)
+
+    def inserir_filho(self, filho):
+        if self.filhos is None:
+            self.filhos = ListaDeNodos()
+        
+        nodo = Nodo(filho)
+        self.filhos.inserir_no_fim(nodo)
 
 class Arvore:
     def __init__(self, conteudo):
--- menu.py
+++ menu.py
@@ -2,5 +2,9 @@ from ed.arvore import Arvore
 
 def main():
     livraria = Arvore('Livros')
-    
+    livraria.imprimir()
+    livraria.raiz.inserir_filho('Gastronomia')
+    livraria.raiz.inserir_filho('Informática')
+    livraria.imprimir()
+
 main()
